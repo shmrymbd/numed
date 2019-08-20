1bea8908d80e71be84b612d5d25d7def0ff61777
shamry <shamry@gmail.com>
2019-08-21 02:54:44 +0800

    add update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-cd /home/dyna/gateway_boardV2
-cp config_file.ini /home/dyna
-cp ./web/config/db.sqlite3 /home/dyna
+cd /home/pi/numed
 mv launcher.sh launcher.sh.bak
 
-sleep 5
-#pkill -f 'python3 gateway.py'
-pkill -f 'python3 manage.py runserver 0.0.0.0:80'
-pkill -f 'python3 watchdog.py'
-
-apt-get install memcached
 git fetch --all
 git reset --hard origin/master
 #git pull
@@ -18,19 +10,13 @@ git reset --hard origin/master
 pip3 install -r requirements.txt
 sleep 2
 pkill -f 'python3 gateway.py'
-pkill -f 'python3 manage.py runserver 0.0.0.0:80'
-pkill -f 'python3 watchdog.py'
 
-cd /home/dyna/
-cp config_file.ini ./gateway_boardV2/
-cp db.sqlite3 ./gateway_boardV2/web/config/
+
+cd /home/pi/
 chown -R dyna:dyna gateway_boardV2/
 
 sleep 2
-cd /home/dyna/gateway_boardV2/
+cd /home/pi/numed/
 python3 gateway.py >/dev/null 2>&1 &
     echo "starting gateway.py....."
-sleep 2
-cd /home/dyna/gateway_boardV2/web/config
-python3 manage.py runserver 0.0.0.0:80 >/dev/null 2>&1 &
-    echo "starting web....."
+
