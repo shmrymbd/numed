92caaed59b06ecbaa6436712fb4920d7d9787657
shamry <shamry@gmail.com>
2019-08-21 13:33:15 +0800

    interval

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/pi/gateway_boardV2
+cd /home/pi/numed
 if pgrep -f 'python3 gateway.py' >/dev/null ;then
     echo "gateway.py already running"
 else python3 gateway.py  >/dev/null 2>&1 &
--- update.sh
+++ update.sh
@@ -8,14 +8,14 @@ git reset --hard origin/master
 
 #comment during testing
 pip3 install -r requirements.txt
-sleep 2
+sleep 1
 pkill -f 'python3 gateway.py'
 
 
 cd /home/pi/
 chown -R pi:pi numed/
 
-sleep 2
+sleep 1
 cd /home/pi/numed/
 python3 gateway.py >/dev/null 2>&1 &
     echo "starting gateway.py....."
